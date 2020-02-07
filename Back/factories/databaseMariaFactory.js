d6dc01474c8435cca50052de09ec5420a88cf5c6
JasLieb <jas.lieb@hotmail.com>
2020-02-07 17:17:32 +0100

    [Back] firstfruit MariaDB migration on interest points

---
+++ Back/factories/databaseMariaFactory.js
@@ -0,0 +1,28 @@
+const maria = require('mariadb');
+
+const pool = maria.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+const query = async (queryString) => {
+    let conn;
+    let res;
+    try {
+
+        conn = await pool.getConnection();
+        res = await conn.query(queryString);
+
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+    return res;
+}
+
+module.exports = {
+    query: query
+}
