311666a8a02884fc32ab267c19cea443be571db1
JasLieb <jas.lieb@hotmail.com>
2020-02-08 14:52:11 +0100

    Prevent nodejs workflow fail

--- Back/factories/databaseMariaFactory.js
+++ Back/factories/databaseMariaFactory.js
@@ -4,7 +4,8 @@ const pool = maria.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 const query = async (queryString) => {
